0b6d3cfdc09a4d73cf9e0d04405b58548b5e2cf2
renechoi <115696395+renechoi@users.noreply.github.com>
2022-11-08 10:30:42 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1108 ms, Memory: 112404 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,28 +1,77 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.OutputStreamWriter;
 
 public class Main {
+	public static int[] inputNumbers;
+	public static int[] sortedArray;
+
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
+		inputNumbers = new int[N];
+		sortedArray = new int[N];
 
-		ArrayList<Integer> list = new ArrayList<>();
-
-		for(int i = 0; i < N; i++) {
-			list.add(Integer.parseInt(br.readLine()));
+		for (int i = 0; i < N; i++) {
+			inputNumbers[i] = Integer.parseInt(br.readLine());
 		}
 
-		Collections.sort(list);		// collections 소트는 merge sort보다 향상된 소트를 수행한다.
+		mergeSort(0, N - 1);
 
-		// 출력부
-		StringBuilder sbAnswer = new StringBuilder();
-		for (int number : list) {
-			sbAnswer.append(number).append("\n");
+		for (int i = 0; i < N; i++) {
+			bw.write(inputNumbers[i] + "\n");
 		}
-		System.out.println(sbAnswer);
+
+		bw.flush();
+		bw.close();
+	}
+
+	private static void mergeSort(int left, int right) {
+		if (right - left < 1)
+			return;
+		int median = left + (right - left) / 2;
+
+		mergeSort(left, median);
+		mergeSort(median + 1, right);
+
+		if (right + 1 - left >= 0) {		// 복사하여 새로운 배열에 넣기 
+			System.arraycopy(inputNumbers, left, sortedArray, left, right + 1 - left);
+		}
+
+		merge(right, median, left);
+	}
+
+	private static void merge(int right, int median, int left) {
+
+		int newLeft = left;
+		int newRight = median + 1;
+
+		while (newLeft <= median && newRight <= right) {
+
+			if (sortedArray[newLeft] > sortedArray[newRight]) {
+				inputNumbers[left] = sortedArray[newRight];
+				left++;
+				newRight++;
+			} else {
+				inputNumbers[left] = sortedArray[newLeft];
+				left++;
+				newLeft++;
+			}
+		}
+
+		while (newLeft <= median) {
+			inputNumbers[left] = sortedArray[newLeft];
+			left++;
+			newLeft++;
+		}
+
+		while (newRight <= right) {
+			inputNumbers[left] = sortedArray[newRight];
+			left++;
+			newRight++;
+		}
 	}
 }
