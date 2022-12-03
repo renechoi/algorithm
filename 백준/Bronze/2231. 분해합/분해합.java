f345229de7db2d4eec3b7664b0dce65151e1f9cc
renechoi <115696395+renechoi@users.noreply.github.com>
2022-12-03 18:23:48 +0900

    [Bronze II] Title: 분해합, Time: 640 ms, Memory: 314552 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,26 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-		int numberToBeFound = Integer.parseInt(bufferedReader.readLine());
-		int answer = 0;
-
-		int generatorMin = numberToBeFound - (getDigit(numberToBeFound) * 9);
-		int generatorMax = createGeneratorNumber((int) Math.pow(10, getDigit(numberToBeFound)) - 1);
-		for (int i = generatorMin; i <= generatorMax; i++) {
-			if (createGeneratorNumber(i) == numberToBeFound) {
-				answer = i;
-				break;
-			}
+		ArrayList<Integer> generatorNumber2 = new ArrayList<>(1000001);
+		for (int i = 0; i < 1000001; i++) {
+			generatorNumber2.add(i, createGeneratorNumber(i));
 		}
 
+		int answer = generatorNumber2.indexOf(Integer.parseInt(bufferedReader.readLine()));
+		if (answer == -1) {
+			answer = 0;
+		}
 		System.out.println(answer);
 	}
+
 
 	private static int getDigit(int number) {
 		return String.valueOf(number).length();
@@ -36,9 +35,8 @@ public class Main {
 	private static int digitSum(int number) {
 		int sum = 0;
 		for (int i = 0; i < getDigit(number); i++) {
-			sum += String.valueOf(number).charAt(i) - 48;
+			sum += String.valueOf(number).charAt(i)-48;
 		}
 		return sum;
 	}
-
 }
