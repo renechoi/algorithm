64b3ab51f5726b8e734a105de5173edabf447a3a
renechoi <115696395+renechoi@users.noreply.github.com>
2022-12-06 06:01:03 +0900

    [Silver III] Title: N과 M (1), Time: 2008 ms, Memory: 51260 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private static int[] sequence;
+	private static boolean[] visited;
+	private static int N;
+	private static int M;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		N = Integer.parseInt(stringTokenizer.nextToken());
+		M = Integer.parseInt(stringTokenizer.nextToken());
+
+		sequence = new int[M];
+		visited = new boolean[N + 1];
+		dfs(0);
+	}
+
+	private static void dfs(int depth) {
+		if (depth == M) {
+			for (int number : sequence) {
+				System.out.print(number + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+
+				sequence[depth] = i;
+				dfs(depth + 1);
+
+				visited[i] = false;
+			}
+		}
+	}
+}
