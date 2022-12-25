10ac52683a91635b0158aee1682173da593efa5f
renechoi <115696395+renechoi@users.noreply.github.com>
2022-12-25 09:42:15 +0900

    [Bronze V] Title: Hook, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def get_input():
+    return sys.stdin.readline()
+
+
+if __name__ == "__main__":
+    print("""#  # #### #### #  #
+#### #  # #  # # #
+#### #  # #  # # #
+#  # #### #### #  #""")
+    
