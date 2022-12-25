dd6522022884a373a1cf17c899b7a3b3b1cf214c
renechoi <115696395+renechoi@users.noreply.github.com>
2022-12-25 09:39:01 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def get_input():
+    return sys.stdin.readline()
+
+
+if __name__ == "__main__":
+    print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE")
+    print("N2 Bomber      Heavy Fighter  Limited    21        ")
+    print("J-Type 327     Light Combat   Unlimited  1         ")
+    print("NX Cruiser     Medium Fighter Limited    18        ")
+    print("N1 Starfighter Medium Fighter Unlimited  25        ")
+    print("Royal Cruiser  Light Combat   Limited    4         ")
+    
