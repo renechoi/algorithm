d11fbf55134addf531a584a0428071b2e494788b
renechoi <115696395+renechoi@users.noreply.github.com>
2022-12-26 12:27:35 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 340 ms, Memory: 19984 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        printStar(N);
+    }
+
+    private static void printStar(int N) {
+        // 별을 찍어보자
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = N - 1; j >= i; j--) {
+                System.out.print(" ");
+            }
+
+            System.out.print("*");
+
+            for (int k = 1; k < (i - 1) * 2; k++) {
+
+                if (i == N) {
+                    for (int l = 1; l <= (N - 1) * 2; l++) {
+                        System.out.print("*");
+                    }
+                    break;
+                }
+
+                System.out.print(" ");
+
+                if (k == ((i - 1) * 2) - 1) {
+                    System.out.print("*");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
