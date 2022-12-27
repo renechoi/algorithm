a29ac18faba3ec3bf07f3808e9ee7622f672e6a5
renechoi <115696395+renechoi@users.noreply.github.com>
2022-12-27 12:40:43 +0900

    [Bronze V] Title: Site Score, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+        int c = Integer.parseInt(stringTokenizer.nextToken());
+        int d = Integer.parseInt(stringTokenizer.nextToken());
+
+        System.out.println(56 * a + 24 * b + 14 * c + 6 * d);
+    }
+}
