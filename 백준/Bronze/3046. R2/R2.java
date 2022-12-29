39fda4c53a47a99de272cbfaf29df3dae4ea07d3
renechoi <115696395+renechoi@users.noreply.github.com>
2022-12-29 09:15:24 +0900

    [Bronze IV] Title: R2, Time: 124 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int c = Integer.parseInt(stringTokenizer.nextToken());
+
+        System.out.println(2 * c - a);
+    }
+}
