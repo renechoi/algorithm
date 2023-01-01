aa1c5104321b47b27ef665e0ed4273b5f8b9166a
renechoi <115696395+renechoi@users.noreply.github.com>
2023-01-01 13:36:57 +0900

    [Bronze V] Title: Julka, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/8437. Julka/Julka.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def get_input():
+    return sys.stdin.readline()
+
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+
+    print((a+b) // 2)
+    print((a-b) // 2)
