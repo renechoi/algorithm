83ae137ddf300a77654b83f713b78f45c27db563
renechoi <115696395+renechoi@users.noreply.github.com>
2023-06-09 16:55:37 +0900

    [Bronze III] Title: ACM 호텔, Time: 136 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,36 +1,27 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
+	private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		int T = Integer.parseInt(bufferedReader.readLine());
 
-		int numberOfTests = Integer.parseInt(stringTokenizer.nextToken());
+		while (T-->0){
+			StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-		while (numberOfTests-- > 0) {
-			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 			int H = Integer.parseInt(stringTokenizer.nextToken());
 			int W = Integer.parseInt(stringTokenizer.nextToken());
-			int person = Integer.parseInt(stringTokenizer.nextToken());
-			findRoomNumber(H, W, person);
-		}
-	}
+			int N = Integer.parseInt(stringTokenizer.nextToken());
 
-	private static void findRoomNumber(int H, int W, int person) {
-		for (int i = 1; i <= W; i++) {
-			for (int j = 1; j <= H; j++) {
-				int roomNumber = j * 100 + i;
-				int count = ((i - 1) * H) + j;
+			int floor = N % H == 0? H * 100 : (N % H) * 100;
+			int room = N % H == 0 ? (N / H) : (N / H) + 1;
 
-				if (count == person) {
-					System.out.println(roomNumber);
-					break;
-				}
-			}
+			System.out.println(floor+room);
 		}
 	}
 }
