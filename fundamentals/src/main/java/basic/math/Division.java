1bb9196167bafd7b27391684b95651979393db5a
renechoi <115696395+renechoi@users.noreply.github.com>
2023-06-18 13:24:23 +0900

    feat: 나눗셈 기본 연산 코드 구현

---
+++ fundamentals/src/main/java/basic/math/Division.java
@@ -0,0 +1,15 @@
+package basic.math;
+
+public class Division {
+	public static void main(String[] args) {
+
+	}
+
+	protected static int getRemainder(int divisor, int dividend){
+		return divisor % dividend;
+	}
+
+	protected static int getQuotient(int divisor, int dividend){
+		return divisor / dividend;
+	}
+}
