633067976c33798ab8eb5709c7349278d7eebee6
renechoi <115696395+renechoi@users.noreply.github.com>
2023-12-31 15:07:21 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1504 ms, Memory: 76972 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,81 +1,68 @@
+import static java.lang.Integer.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	/**
-	 * 인접 리스트 자료구조를 통해 주어지는 데이터를 입력한다
-	 * <p>
-	 * 루트를 1로 지정해주므로 1번 노드부터 dfs로 탐색한다
-	 * <p>
-	 * i) 인접 리스트로 트리 데이터 받기
-	 * ii) dfs 탐색 -> 수행하며 부모 노드의 값을 정답 배열에 저장
-	 * iii) 정답 배열의 2번 인덱스부터 출력
-	 */
 
-	static int numberOfNodes;
-	static boolean[] visitedNodes;
-	static ArrayList<Integer>[] tree;
-	static int[] answers;
+	private static List<List<Integer>> adjList;
+	private static int[] parents;
+	private static boolean[] visited;
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		numberOfNodes = scanner.nextInt();
-
-		visitedNodes = new boolean[numberOfNodes + 1];
-		answers = new int[numberOfNodes + 1];
-
-		initializeTree();
-		saveNodes(scanner);
-
-		dfs(1);
-		drawAnswer();
+	private static StringTokenizer receiveInput(BufferedReader bufferedReader) throws IOException {
+		return new StringTokenizer(bufferedReader.readLine());
 	}
 
-	private static void drawAnswer() {
-		for (int i = 2; i <= numberOfNodes; i++) {
-			System.out.println(answers[i]);
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		// BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"));
+
+		StringTokenizer stringTokenizer = receiveInput(bufferedReader);
+		int N = parseInt(stringTokenizer.nextToken());
+
+		adjList = new ArrayList<>();
+		for (int i = 0; i <= N; i++) {
+			adjList.add(new ArrayList<>());
 		}
-	}
 
-	private static void saveNodes(Scanner scanner) {
-		for (int i = 1; i < numberOfNodes; i++) {            // e.g. 1 6
-			int nodeConnected1 = scanner.nextInt();            // => 1
-			int nodeConnected2 = scanner.nextInt();        // => 6
-			tree[nodeConnected1].add(nodeConnected2);
-			tree[nodeConnected2].add(nodeConnected1);        // => 1 => 6, 6 => 1
+		for (int i = 0; i < N - 1; i++) {
+			stringTokenizer = receiveInput(bufferedReader);
+			int a = Integer.parseInt(stringTokenizer.nextToken());
+			int b = Integer.parseInt(stringTokenizer.nextToken());
+			adjList.get(a).add(b);
+			adjList.get(b).add(a);
 		}
-	}
 
-	private static void initializeTree() {
-		tree = new ArrayList[numberOfNodes + 1];
-		for (int i = 0; i < tree.length; i++) {
-			tree[i] = new ArrayList<>();
+		parents = new int[N + 1];
+		visited = new boolean[N + 1];
+
+		findParents(1);
+
+		for (int i = 2; i <= N; i++) {
+			System.out.println(parents[i]);
 		}
+
 	}
 
-	static void dfs(int number) {
-		visitedNodes[number] = true;
-		for (int node : tree[number]) {
-			if (!visitedNodes[node]) {
-				answers[node] = number;
-				dfs(node);
+	private static void findParents(int currentNode) {
+		// 현재 노드를 방문 처리
+		visited[currentNode] = true;
+
+		// 인접 리스트를 순회
+		for (int adjacent : adjList.get(currentNode)) {
+			// 인접 노드가 방문되지 않았다면
+			if (!visited[adjacent]) {
+				// 인접 노드의 부모를 현재 노드로 설정
+				parents[adjacent] = currentNode;
+				// 인접 노드를 방문
+				findParents(adjacent);
 			}
 		}
-		
-		/**
-		 * e.g 
-		 * 1 시작 => 6 ,4 중 6 탐색 
-		 * 6 => 방문 x => 1번으로 부모 노드 업데이트 
-		 * 6 => 1, 3 중 1 탐색 
-		 * => 방문 o => 3 탐색  
-		 * 3 => 방문 x => 6번으로 부모 노드 업데이트 
-		 * 3 => 6, 5 탐색 중 6번 방문 o => 5 탐색 
-		 * 5 => 방문 x => 3으로 부모 노드 업데이트
-		 * => leaf 노드이므로 다시 올라가면서 탐색 
-		 * => 3, 7, 1 => 
-		 * 1 = > 4 탐색 
-		 * => 같은 로직으로 탐색 
-		 */
 	}
 }
