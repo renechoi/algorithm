c998613af6047a37dc0a70206080600bc393bf1d
renechoi <115696395+renechoi@users.noreply.github.com>
2024-09-22 10:57:43 +0900

    [level 2] Title: 올바른 괄호, Time: 19.49 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,10 +1,29 @@
+import java.util.LinkedList;
+import java.util.List;
+
 class Solution {
+
     boolean solution(String s) {
-        boolean answer = true;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        List<Character> list = new LinkedList<>();
 
-        return answer;
+        char[] brackets = s.toCharArray();
+        for (char bracket : brackets) {
+
+            if (bracket == '(') {
+                // 중간에 삽입할 경우 성능 저하 (O(n))
+                list.add(0, bracket);  // 비효율적인 삽입
+                continue;
+            }
+
+            if (bracket == ')' && !list.isEmpty()) {
+                // 중간에서 삭제할 경우 성능 저하 (O(n))
+                list.remove(list.size() - 1);  // 비효율적인 삭제
+            } else {
+                return false;
+            }
+        }
+
+        return list.isEmpty();
     }
 }
