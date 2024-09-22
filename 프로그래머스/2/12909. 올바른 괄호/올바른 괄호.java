9d915b3ad09df52ddb995ae0e5f5c8201d3150f1
renechoi <115696395+renechoi@users.noreply.github.com>
2024-09-22 10:53:30 +0900

    [level 2] Title: 올바른 괄호, Time: 17.07 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,27 +1,10 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
 
-import java.util.*;
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
-public class Solution {
-
-	boolean solution(String s) {
-
-		Stack<Character> stack = new Stack<>();
-
-		char[] brackets = s.toCharArray();
-		for (char bracket : brackets) {
-
-			if (bracket == '(') {
-				stack.add(bracket);
-				continue;
-			}
-
-			if (bracket == ')' && !stack.isEmpty()) {
-				stack.pop();
-			} else {
-				return false;
-			}
-		}
-
-		return stack.isEmpty();
-	}
+        return answer;
+    }
 }
