db52c638052c4a8aff93602dce2d2b92473f1e7f
renechoi <115696395+renechoi@users.noreply.github.com>
2024-10-06 11:49:03 +0900

    [Silver IV] Title: 큐, Time: 160 ms, Memory: 19788 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,83 +1,115 @@
+
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
+
 
 public class Main {
 
-    private static Deque<Integer> queue;
-    private static StringBuilder stringBuilder;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		int 명령어개수 = Integer.parseInt(br.readLine());
+		배열로구현한선형큐 큐 = new 배열로구현한선형큐(100000);
 
-        int testCase = Integer.parseInt(bufferedReader.readLine());
-        queue = new ArrayDeque<>();
+		StringBuilder stringBuilder = new StringBuilder();
+		while (명령어개수-- > 0) {
+			StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+			String 명령어 = stringTokenizer.nextToken();
 
-        stringBuilder = new StringBuilder();
-        for (int i = 0; i < testCase; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            String command = stringTokenizer.nextToken();
-            switch (command) {
-                case "push":
-                    handlePushCommand(Integer.parseInt(stringTokenizer.nextToken()));
-                    continue;
-                case "pop":
-                    stringBuilder.append(handlePopCommand()).append("\n");
-                    continue;
-                case "size":
-                    stringBuilder.append(handleSizeCommand()).append("\n");
-                    continue;
-                case "empty":
-                    stringBuilder.append(handleEmptyCommand()).append("\n");
-                    continue;
-                case "front":
-                    stringBuilder.append(handleFrontCommand()).append("\n");
-                    continue;
-                case "back":
-                    stringBuilder.append(handleBackCommand()).append("\n");
-            }
-        }
-        System.out.println(stringBuilder);
-    }
+			switch (명령어) {
+				case "push":
+					큐.add(Integer.parseInt(stringTokenizer.nextToken()));
+					break;
+				case "pop":
+					stringBuilder.append(큐.poll()).append("\n");
+					break;
+				case "size":
+					stringBuilder.append(큐.size()).append("\n");
+					break;
+				case "empty":
+					stringBuilder.append(큐.isEmpty() ? 1 : 0).append("\n");
+					break;
+				case "front":
+					stringBuilder.append(큐.front()).append("\n");
+					break;
+				case "back":
+					stringBuilder.append(큐.back()).append("\n");
+					break;
+			}
+		}
+
+		System.out.println(stringBuilder);
+
+	}
 
 
-    private static void handlePushCommand(int number) {
-        queue.add(number);
-    }
 
-    private static Integer handlePopCommand() {
-        try {
-            return queue.pop();
-        } catch (NoSuchElementException e) {
-            return -1;
-        }
-    }
 
-    private static Integer handleSizeCommand() {
-        return queue.size();
-    }
+	public static class 배열로구현한선형큐 {
 
-    private static Integer handleEmptyCommand() {
-        if (queue.isEmpty()) {
-            return 1;
-        }
-        return 0;
-    }
+		private int front;
+		private int rear;
+		private int[] 배열;
 
-    private static Integer handleFrontCommand() {
-        try {
-            return queue.getFirst();
-        } catch (NoSuchElementException e) {
-            return -1;
-        }
-    }
+		private int size;
+		private int capacity;
 
-    private static Integer handleBackCommand() {
-        try {
-            return queue.getLast();
-        } catch (NoSuchElementException e) {
-            return -1;
-        }
-    }
+		public 배열로구현한선형큐(int capacity) {
+			this.배열 = new int[capacity];
+			this.capacity = capacity;
+			this.front = 0;
+			this.rear = 0;
+			this.size = 0;
+		}
+
+		public boolean isEmpty() {
+			return this.size == 0;
+		}
+
+		public boolean isFull() {
+			return this.size == capacity;
+		}
+
+		public int poll() {
+			if (isEmpty()) {
+				return -1;
+			}
+			int value = 배열[front];
+			front++;
+			size--;
+			return value;
+		}
+
+		public void add(int x) {
+			if (isFull()) {
+				throw new RuntimeException();
+			}
+			배열[rear++] = x;
+			size++;
+		}
+
+		public int size() {
+			return this.size;
+		}
+
+		public int front() {
+			if (isEmpty()) {
+				return -1;
+			}
+			return 배열[front];
+		}
+
+		public int back() {
+			if (isEmpty()) {
+				return -1;
+			}
+			return 배열[rear - 1];
+		}
+	}
+
 }
+
+
