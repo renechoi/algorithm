f93c3ad1b7221750970b4bf2112435d1a449bcc7
renechoi <115696395+renechoi@users.noreply.github.com>
2024-10-12 15:36:19 +0900

    [Silver III] Title: 기념품, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/12873. 기념품/기념품.java
+++ 백준/Silver/12873. 기념품/기념품.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -18,100 +16,14 @@ public class Main {
 		System.out.println(당첨자구하기(사람수));
 
 	}
-
-	// private static int 당첨자구하기(int 사람수) {
-	//
-	// 	ArrayDeque<Integer> 원형큐 = new ArrayDeque<>();
-	//
-	// 	for (int i = 1; i <= 사람수; i++) {
-	// 		원형큐.add(i);
-	// 	}
-	//
-	// 	단계 단계 = new 단계(1);
-	// 	while (원형큐.size() > 1) {
-	//
-	// 		단계.단계별횟수차감();
-	// 		if (단계.단계별횟수 == 0) {
-	// 			원형큐.pop();
-	// 			단계.단계업();
-	// 		} else {
-	// 			원형큐.add(원형큐.pop());
-	// 		}
-	// 	}
-	//
-	// 	return 원형큐.pop();
-	// }
-
-	private static int 당첨자구하기(int 사람수) {
-		ArrayDeque<Integer> 원형큐 = new ArrayDeque<>();
-		for (int i = 1; i <= 사람수; i++) {
-			원형큐.add(i);
+    
+    private static int 당첨자구하기(int 사람수) {
+		int 당첨자 = 0; // 초기 당첨자 위치 (0 기반 인덱싱)
+		for (int n = 2; n <= 사람수; n++) {
+			int t = 사람수 - n + 1; // 단계 번호를 1부터 사람수-1까지 증가
+			long 단계별횟수 = (long) t * t * t; 
+			당첨자 = (int) ((당첨자 + 단계별횟수) % n); 
 		}
-
-		long 현재위치 = 0;
-		단계 단계 = new 단계(1);
-
-		while (원형큐.size() > 1) {
-			현재위치 = (현재위치 + 단계.단계별횟수 - 1) % 원형큐.size();
-
-			// 큐에서 해당 위치만큼 이동한 후 제거 (앞으로 빼오기)
-			for (int i = 0; i < 현재위치; i++) {
-				원형큐.addLast(원형큐.removeFirst());
-			}
-
-			원형큐.poll();
-
-
-			현재위치 = 0; // 위치가 하나 줄어들었으므로 현재 위치는 0으로 리셋
-
-			단계.단계업();
-		}
-
-		return 원형큐.poll();
+		return 당첨자 + 1; // 1부터 시작하는 인덱스로 변환하여 반환
 	}
-
-
-	// private static int 당첨자구하기(int 사람수) {
-	// 	ArrayList<Integer> 참가자 = new ArrayList<>();
-	// 	for (int i = 1; i <= 사람수; i++) {
-	// 		참가자.add(i);
-	// 	}
-	//
-	// 	int 현재위치 = 0;
-	// 	단계 단계 = new 단계(1);
-	//
-	// 	while (참가자.size() > 1) {
-	// 		현재위치 = (현재위치 + 단계.단계별횟수 - 1) % 참가자.size();
-	// 		참가자.remove(현재위치); // 해당 위치에 있는 참가자를 바로 빼주기
-	// 		단계.단계업();
-	// 	}
-	//
-	// 	return 참가자.get(0); 		// 마지막 남은 참가자의 번호 반환
-	// }
-
-
-	public static class 단계 {
-		private int 단계;
-		private long 단계별횟수;
-
-		public 단계(int 단계) {
-			this.단계 = 단계;
-			this.단계별횟수 = get단계별횟수(단계);
-		}
-
-		private long get단계별횟수(int 단계) {
-			return (long) 단계 * 단계 * 단계;
-		}
-
-		public void 단계별횟수차감() {
-			this.단계별횟수--;
-		}
-
-		public void 단계업() {
-			this.단계++;
-			this.단계별횟수 = get단계별횟수(단계);
-		}
-	}
-
-
 }
