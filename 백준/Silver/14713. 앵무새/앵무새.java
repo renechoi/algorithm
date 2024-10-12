7fc063c497d6dcb297a88df1d775d594dc2ab4e1
renechoi <115696395+renechoi@users.noreply.github.com>
2024-10-12 12:02:13 +0900

    [Silver II] Title: 앵무새, Time: 284 ms, Memory: 22492 KB -BaekjoonHub

--- 백준/Silver/14713. 앵무새/앵무새.java
+++ 백준/Silver/14713. 앵무새/앵무새.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 
@@ -23,50 +27,56 @@ public class Main {
 		String 검증해야하는문장 = br.readLine();
 
 		System.out.println(가능여부판단하기(앵무새가말한문장들, 검증해야하는문장));
-
 	}
 
 	private static String 가능여부판단하기(ArrayList<String> 앵무새가말한문장들, String 검증해야하는문장) {
 
-		 ArrayDeque<String>[] 앵무새별큐 = new ArrayDeque[앵무새가말한문장들.size()];
-		 for(int i = 0; i<앵무새별큐.length; i++){
-			 앵무새별큐[i]=new ArrayDeque<>();
-		 }
+		int N = 앵무새가말한문장들.size();
 
-		for (int i = 0; i < 앵무새가말한문장들.size(); i++ ){
-			StringTokenizer stringTokenizer = new StringTokenizer(앵무새가말한문장들.get(i));
+		List<String>[] 앵무새문장 = new List[N];
+		int[] 앵무새인덱스 = new int[N];
 
-			while(stringTokenizer.hasMoreTokens()){
-				앵무새별큐[i].add(stringTokenizer.nextToken());
+		for (int i = 0; i < N; i++) {
+			String[] words = 앵무새가말한문장들.get(i).split("\\s+");
+			앵무새문장[i] = new ArrayList<>(Arrays.asList(words));
+			앵무새인덱스[i] = 0;
+		}
+
+		Map<String, Integer> 단어별앵무새맵 = new HashMap<>();
+
+		for (int i = 0; i < N; i++) {
+			if (앵무새문장[i].size() > 0) {
+				String word = 앵무새문장[i].get(0);
+				단어별앵무새맵.put(word, i);
 			}
 		}
 
-		StringTokenizer stringTokenizer = new StringTokenizer(검증해야하는문장);
+		StringTokenizer st = new StringTokenizer(검증해야하는문장);
 
-		while (stringTokenizer.hasMoreTokens()) {
-			String 단어 = stringTokenizer.nextToken();
+		while (st.hasMoreTokens()) {
+			String 단어 = st.nextToken();
 
-			boolean 단어match = false;
-			for (int i = 0; i < 앵무새별큐.length; i++) {
-				if (단어.equals(앵무새별큐[i].peek())) {
-					앵무새별큐[i].pop();
-					단어match = true;
-					break;
+			if (단어별앵무새맵.containsKey(단어)) {
+				int 앵무새번호 = 단어별앵무새맵.get(단어);
+
+				앵무새인덱스[앵무새번호]++;
+
+				단어별앵무새맵.remove(단어);
+
+				if (앵무새인덱스[앵무새번호] < 앵무새문장[앵무새번호].size()) {
+					String 다음단어 = 앵무새문장[앵무새번호].get(앵무새인덱스[앵무새번호]);
+					단어별앵무새맵.put(다음단어, 앵무새번호);
 				}
-			}
-
-			if (!단어match) {
+			} else {
 				return "Impossible";
 			}
 		}
 
-		// 모든 앵무새의 큐가 비었는지 확인
-		for (ArrayDeque<String> 큐 : 앵무새별큐) {
-			if (!큐.isEmpty()) {
+		for (int i = 0; i < N; i++) {
+			if (앵무새인덱스[i] != 앵무새문장[i].size()) {
 				return "Impossible";
 			}
 		}
-
 
 		return "Possible";
 	}
