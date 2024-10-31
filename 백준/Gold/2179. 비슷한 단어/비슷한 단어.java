febf258752fa9df534f596b3338eb7d505d17725
renechoi <115696395+renechoi@users.noreply.github.com>
2024-10-31 21:11:54 +0900

    [Gold IV] Title: 비슷한 단어, Time: 248 ms, Memory: 28168 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -1,80 +1,76 @@
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-
-
+import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        List<단어> 단어들 = new ArrayList<>();
-        for(int i = 0; i < N; i++) {
-            단어들.add(new 단어(br.readLine(), i));
-        }
-        
-        int 가장높은유사도 = -1;
-        단어 결과단어1 = null;
-        단어 결과단어2 = null;
-        
-        for(int i = 0; i < N; i++) {
-            for(int j = i + 1; j < N; j++) {
-                단어 첫번째단어 = 단어들.get(i);
-                단어 두번째단어 = 단어들.get(j);
-                
-                int 현재유사도 = get유사도(첫번째단어.value, 두번째단어.value);
-                
-                if(현재유사도 > 가장높은유사도) {
-                    가장높은유사도 = 현재유사도;
-                    결과단어1 = 첫번째단어;
-                    결과단어2 = 두번째단어;
-                }
-                else if(현재유사도 == 가장높은유사도) {
-                    if(첫번째단어.index < 결과단어1.index || 
-                       (첫번째단어.index == 결과단어1.index && 두번째단어.index < 결과단어2.index)) {
-                        결과단어1 = 첫번째단어;
-                        결과단어2 = 두번째단어;
-                    }
-                }
-            }
-        }
-        
-        System.out.println(결과단어1.value);
-        System.out.println(결과단어2.value);
-    }
-    
-    static class 단어 {
-        String value;
-        int index;
-        
-        public 단어(String value, int index) {
-            this.value = value;
-            this.index = index;
-        }
-    }
-    
-    static int get유사도(String str1, String str2) {
-        int minLen = Math.min(str1.length(), str2.length());
-        int 접두사길이 = 0;
-        
-        for(int i = 0; i < minLen; i++) {
-            if(str1.charAt(i) == str2.charAt(i)) {
-                접두사길이++;
-            } else {
-                break;
-            }
-        }
-        return 접두사길이;
-    }
+
+	public static void main(String[] args) throws IOException {
+		// System.setIn(new FileInputStream("fundamentals/src/test/java/template/Boj/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int 최장길이 = -1;
+
+		HashMap<String, 단어> map = new HashMap<>();
+		단어 S = null;
+		단어 T = null;
+
+		for (int i = 0; i < N; i++) {
+			String 단어내용 = br.readLine();
+			단어 현재단어 = new 단어(단어내용, i);
+
+			for (int j = 0; j < 단어내용.length(); j++) {
+				String 접두사 = 단어내용.substring(0, j + 1);
+
+				if (map.containsKey(접두사)) {
+					단어 이전단어 = map.get(접두사);
+
+					if (접두사.length() > 최장길이) {
+						S = 이전단어;
+						T = 현재단어;
+						최장길이 = 접두사.length();
+					}
+
+					else if (접두사길이가같고(접두사, 최장길이) && 현재단어가인덱스순으로앞선다면(이전단어, S, 현재단어, T)) {
+
+						S = 이전단어;
+						T = 현재단어;
+					}
+
+				} else {
+					map.put(접두사, 현재단어);
+				}
+			}
+		}
+
+		System.out.println(S.value);
+		System.out.println(T.value);
+	}
+
+	private static boolean 접두사길이가같고(String 접두사, int 최장길이) {
+		return 접두사.length() == 최장길이;
+	}
+
+	private static boolean 현재단어가인덱스순으로앞선다면(단어 이전단어, 단어 S, 단어 현재단어, 단어 T) {
+		return 이전단어.index < S.index || (이전단어.index == S.index && 현재단어.index < T.index);
+	}
+
+	public static class 단어 {
+		String value;
+		int index;
+
+		public 단어(String value, int index) {
+			this.value = value;
+			this.index = index;
+		}
+	}
+
+
 }
+
+
