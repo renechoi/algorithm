e3ee00ee06ac6c1cc585630152e3ee119c136ceb
renechoi <115696395+renechoi@users.noreply.github.com>
2025-01-12 11:14:39 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.04 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -1,33 +1,6 @@
-
-import java.util.ArrayList;
-
-public class Solution {
-	final int[] dx = {-1, 1, 0, 0};
-	final int[] dy = {0, 0, -1, 1};
-
-	public int solution(String[][] board, int h, int w) {
-
-		int count =0;
-
-		for (int i = 0; i < 4; i++) {
-			int nx = h + dx[i];
-			int ny = w + dy[i];
-
-			if (범위를벗어났다면(nx, ny, board)) {
-				continue;
-			}
-
-			if (board[nx][ny].equals(board[h][w])) {
-				count++;
-			}
-		}
-
-		return count;
-	}
-
-	private boolean 범위를벗어났다면(int nx, int ny, String[][] board) {
-    return (nx < 0 || nx >= board.length) || (ny < 0 || ny >= board[0].length);
-}
-
-	
+class Solution {
+    public int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        return answer;
+    }
 }
