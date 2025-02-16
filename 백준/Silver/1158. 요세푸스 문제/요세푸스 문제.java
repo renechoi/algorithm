b285bf1e2134b26df2243a33b631e0b336ffdb4a
renechoi <115696395+renechoi@users.noreply.github.com>
2025-02-16 11:20:06 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 112 ms, Memory: 14740 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,47 +1,55 @@
+
 import static java.lang.Integer.*;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class Main {
 
-	private static StringTokenizer receiveInput(BufferedReader bufferedReader) throws IOException {
-		return new StringTokenizer(bufferedReader.readLine());
-	}
+
+
 
 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
 
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		// BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"));
-
-		StringTokenizer stringTokenizer = receiveInput(bufferedReader);
 		int N = parseInt(stringTokenizer.nextToken());
 		int K = parseInt(stringTokenizer.nextToken());
+
 
 		List<Integer> circle = new ArrayList<>();
 		for (int i = 1; i <= N; i++) {
 			circle.add(i);
 		}
 
-		StringBuilder result = new StringBuilder();
-		result.append("<");
+		StringBuilder sb = new StringBuilder();
 
 		int index = 0;
+
+		sb.append("<");
+
 		while (!circle.isEmpty()) {
 			index = (index + K - 1) % circle.size();
-			result.append(circle.remove(index));
+			sb.append(circle.remove(index));
 			if (!circle.isEmpty()) {
-				result.append(", ");
+				sb.append(", ");
 			}
 		}
-		result.append(">");
 
-		System.out.println(result);
+		sb.append(">");
+		System.out.println(sb);
 	}
+
 }
