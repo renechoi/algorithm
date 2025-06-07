15c033e0110c885a1bbf6d0863affde0092ee956
renechoi <115696395+renechoi@users.noreply.github.com>
2025-06-07 10:08:39 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 320 ms, Memory: 31040 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,152 +1,64 @@
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int n;
-	static int m;
-	static 좌표[][] map;
+    static int N, M;
+    static char[][] campus;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(stringTokenizer.nextToken());
-		m = Integer.parseInt(stringTokenizer.nextToken());
-		map = new 좌표[n][m];
-		좌표 start = null;
-		for (int i = 0; i < n; i++) {
-			stringTokenizer = new StringTokenizer(br.readLine());
-			String trim = stringTokenizer.nextToken().trim();
-			for (int j = 0; j < m; j++) {
-				String type = String.valueOf(trim.charAt(j));
-				map[i][j] = new 좌표(i, j, type);
-				if (type.equals("I")) {
-					start = map[i][j];
-				}
-			}
-		}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		int result = bfs(start);
-		if (result == 0) {
-			System.out.println("TT");
-		} else {
-			System.out.println(result);
-		}
-	}
+        campus = new char[N][M];
+        visited = new boolean[N][M];
 
-	
-	public static int bfs(좌표 start){
-		Queue <좌표> queue = new ArrayDeque<>();
-		queue.offer(start);
-		int count = 0;
-		while (!queue.isEmpty()) {
-			좌표 cur = queue.poll();
-			if (cur.visited || cur.is벽()) {
-				continue;
-			}
+        int startX = 0, startY = 0;
 
-			cur.markVisited();
+        for (int i = 0; i < N; i++) {
+            campus[i] = br.readLine().toCharArray();
+            for (int j = 0; j < M; j++) {
+                if (campus[i][j] == 'I') {
+                    startX = i;
+                    startY = j;
+                }
+            }
+        }
 
-			if (cur.is사람()) {
-				count++;
-			}
+        int people = dfs(startX, startY);
 
-			for (좌표 nxt : cur.getAdjacent()) {
-				if (!nxt.visited && !nxt.is벽()) {
-					queue.offer(nxt);
-				}
-			}
-		}
-		return count;
-	}
+        System.out.println(people == 0 ? "TT" : people);
+    }
 
+    public static int dfs(int x, int y) {
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{x, y});
+        visited[x][y] = true;
+        int count = 0;
 
-	/**
-	 * 시간 초과
-	 */
-	public static int dfs(좌표 cur) {
-		if (cur.visited || cur.is벽()) {
-			return 0;
-		}
+        while (!stack.isEmpty()) {
+            int[] cur = stack.pop();
 
-		cur.markVisited();
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
 
-		int count = 0;
-		if (cur.is사람()) {
-			count++;
-		}
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny] || campus[nx][ny] == 'X')
+                    continue;
 
-		for (좌표 nxt : cur.getAdjacent()) {
-			if (!nxt.visited && !nxt.is벽()) {
-				count += dfs(nxt);
-			}
-		}
+                if (campus[nx][ny] == 'P') count++;
 
-		return count;
-	}
+                visited[nx][ny] = true;
+                stack.push(new int[]{nx, ny});
+            }
+        }
 
-
-
-
-
-	public static class 좌표 {
-		int x;
-		int y;
-		String  type;
-		boolean visited;
-
-		public 좌표(int x, int y, String type) {
-			this.x = x;
-			this.y = y;
-			this.type = type;
-			this.visited = false;
-		}
-
-		private boolean is도연() {
-			return type.equals("I");
-		}
-
-		private boolean is벽() {
-			return type.equals("X");
-		}
-
-		private boolean is길() {
-			return type.equals("O");
-		}
-
-		private boolean is사람() {
-			return type.equals("P");
-		}
-
-		public List<좌표> getAdjacent() {
-			List<좌표> neighbors = new ArrayList<>(4);
-			int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-
-			for (int[] d : dirs) {
-				int nx = this.x + d[0];
-				int ny = this.y + d[1];
-				if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-					neighbors.add(map[nx][ny]);
-				}
-			}
-			return neighbors;
-		}
-
-		public void markVisited() {
-			this.visited = true;
-		}
-	}
-
+        return count;
+    }
 }
