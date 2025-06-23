d6b2493d41aee8e5c5c84b725a49560882f73878
renechoi <115696395+renechoi@users.noreply.github.com>
2025-06-23 23:06:34 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 2, Time: 376 ms, Memory: 18464 KB -BaekjoonHub

---
+++ 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int 총카운트;
+	static int K;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(stringTokenizer.nextToken());
+		K = Integer.parseInt(stringTokenizer.nextToken());
+		int[] ints = new int[N];
+		stringTokenizer = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			ints[i] = Integer.parseInt(stringTokenizer.nextToken());
+		}
+
+		버블정렬(ints);
+		if (총카운트 < K) {
+			sb.append(-1);
+		}
+		System.out.println(sb.toString());
+	}
+
+	public static void 버블정렬(int[] arr) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					총카운트++;
+					swap(arr, j, j + 1);
+					if(총카운트 == K) {
+						Arrays.stream(arr).forEach(value -> sb.append(value).append(" "));
+					}
+				}
+			}
+		}
+	}
+
+	public static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+
+}
