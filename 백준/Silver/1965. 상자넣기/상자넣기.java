495b34a05baf3aec6451a97d3a18e06b7e23b694
renechoi <115696395+renechoi@users.noreply.github.com>
2025-10-24 20:18:38 +0900

    [Silver II] Title: 상자넣기, Time: 120 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] boxes = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            boxes[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        
+        int maxLIS = 0; 
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1; 
+            
+            for (int j = 0; j < i; j++) {
+                
+                if (boxes[j] < boxes[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            
+            maxLIS = Math.max(maxLIS, dp[i]);
+        }
+
+        System.out.println(maxLIS);
+    }
+}
